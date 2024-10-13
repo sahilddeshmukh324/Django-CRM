da5abb5b0ce7df954efc78bd95aa0b7bb543960f
Sahil Deshmukh <sahildeshmukh324@gmail.com>
2024-10-13 10:33:41 +0530

    Login Commit 13/10

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,9 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    # path('login/',views.login_user,name='login'),
+    # path('logout',views.logout_user,name='logout'),
 
 ]
 
